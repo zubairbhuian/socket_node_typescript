f81703007ad5c47432f139e98fd351bd29956094
Zubair <74538410+zubairbhuian@users.noreply.github.com>
2023-10-28 00:19:11 +0600

    base

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express,{Request ,Response} from "express"
+
+
+const app =express();
+
+
+app.get('/',(req:Request,res:Response)=>{
+    res.send({"name":"Zubair"});
+
+})
+
+
+app.listen(4000,()=>{
+    console.log("Server is running 5000 port");
+})
+
+
